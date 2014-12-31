65a3f743d6a35b731ef372d46f77963227767a96
kingofkung <bjr@BJRs-MacBook-Pro.local>
2014-12-31 17:58:39 -0600

    Changed datasim to reflect logit data gen process

--- R/Datasim.R
+++ R/Datasim.R
@@ -12,8 +12,10 @@ set.seed(12345) # so it's replicable
 
 # make empty matrix where we can store IVs
 IVdat <-  data.frame(matrix(data = NA, ncol = 10, nrow = 50))
-IVdat <- as.data.frame( lapply(IVdat, function(x) x <- rnorm(nrow(IVdat)))) #Fill that frame with data
-dvdat <- as.data.frame(rbinom(nrow(IVdat), size = 1, prob = 0.5))
+IVdat <- as.data.frame(lapply(IVdat, function(x) x <- rnorm(nrow(IVdat)))) #Fill that frame with data
+
+
+
 
 seq1 <- 1:nrow(IVdat)
 seq2 <- 1:nrow(IVdat) * 2
@@ -23,14 +25,24 @@ seq7 <- seq2/2 * 7
 # devseq <- seq2
 
 
+
+
 seqlist <- list(seq1, seq2, negseq, countdown, seq7)
 
-IVsamp <- sample(length(IVdat), length(seqlist)) #create and store a sample of the columns equivalent to the length of seqlist, so that we can affect only those columns.
+# IVsamp <- sample(length(IVdat), length(seqlist)) #create and store a sample of the columns equivalent to the length of seqlist, so that we can affect only those columns.
+IVsamp <- c(3,2,6,7,8) #No reason to keep generating this.
+
+
 
 IVdat[, IVsamp] <-  IVdat[, IVsamp] + as.data.frame(seqlist) #and add the information
 
 # dvdat <- dvdat + devseq
-names(dvdat) <- 'DV'
+
+relation <- IVdat[,4]
+
+dvdat <- data.frame('DV' = exp(relation)/(1 + exp(relation))) #Get into logistic form
+dvdat <-  ifelse(dvdat >= .5, 1, 0)
+
 # #begin working on BeSiVa guts
 
 
