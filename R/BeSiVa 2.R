7c20a63b0137a01efabed2944ec5cc495a36f427
kingofkung <bjr@BJRs-MacBook-Pro.local>
2015-01-01 22:06:12 -0600

    Further issue
    For some reason, they’re all being predicted high. I don’t see why.

--- R/BeSiVa 2.R
+++ R/BeSiVa 2.R
@@ -51,21 +51,23 @@ df <- fdf
 	}
 	
 			ifelse( is.null(aivnames) == T, {LapIvys <- Ivynames; offsettr <- 0}, {LapIvys <-  c('', Ivynames); offsettr <- 1}) #if there are no aivs mentioned, make sure the first regression includes an IV. If there are, make sure that the first regression is blank (to account for only the aivs). In addition, create a value for offsettr
-	
+ datset	 <- subsetter(df)
 	
 	 # for(u in 1:niter){ #determines number of rounds of variable consideration (NVar).
 		
 		#lapply function that loops over all independent variables in Ivys and makes a linear regression with them
-		singregs <-  lapply(LapIvys, FUN = function(col, dvname = Deevname, aivees = aivnames, famiglia = fam, alldat = df[-subsetter(df),], ptdat = df[subsetter(df),]){
+		singregs <-  lapply(LapIvys, FUN = function(col, dvname = Deevname, aivees = aivnames, famiglia = fam, alldat = df[-datset,], ptdat = df[datset,]){
 			
 			ifelse(col == '', Ivform <-  paste(aivees, collapse = ' + '), #if there's nothing in the column, paste the aivs together.
 			Ivform <-  paste(paste(aivees, collapse = '+'), col, sep = ' + ')) #otherwise, add in the new column and paste that into Ivform
 			if(is.null(aivnames) == T ) print(Ivform <-  gsub('[+]', '', Ivform)) #makes it so if there are no IVs, the extra + sign is scrubbed from Ivform
 			 
 			print(form <- paste(dvname, '~', Ivform)) #Make and store a formula with IVfom and dvname as text together
-			reg <-  glm(as.formula(form), data = alldat, family = famiglia)	#Perform the regression
+			reg <-  glm(as.formula(form), family = famiglia, data = alldat)	#Perform the regression
 			print(summary(reg))
-			crit <-  critergen(predict(reg, ptdat), ptdat[,dvname], fulltabl = F) #generate the criterion.
+			prediction <- predict(reg, ptdat, type = 'response')
+			print(data.frame( prediction, dv = ptdat[,dvname]))
+			crit <-  critergen(prediction,  ptdat[,dvname], fulltabl = F) #generate the criterion.
 				#Just realized, critergen will need to be changed if we ever want to use it on something else. Residuals should work for continuous, but need to brush up on deviance. 
 			rm(reg)
 			crit
--- R/Datasim.R
+++ R/Datasim.R
@@ -11,7 +11,7 @@ rm(list = ls())
 set.seed(123456) # so it's replicable
 
 # make empty matrix where we can store IVs
-IVdat <-  data.frame(matrix(data = NA, ncol = 10, nrow = 100))
+IVdat <-  data.frame(matrix(data = NA, ncol = 10, nrow = 117))
 IVdat <- as.data.frame(lapply(IVdat, function(x) x <- rnorm(nrow(IVdat), mean = 0, sd = 7))) #Fill that frame with data
 
 
@@ -38,7 +38,7 @@ IVdat[, IVsamp] <-  IVdat[, IVsamp] + as.data.frame(seqlist) #and add the inform
 
 # dvdat <- dvdat + devseq
 
-relation <- seq2 + seq4 + rnorm(nrow(IVdat), mean = 0, sd = .01)
+relation <- 2 + seq2 + seq4 
 
 dvdat <- data.frame('DV' = exp(relation)/(1 + exp(relation))) #Get into logistic form
 dvdat <-  ifelse(dvdat >= .5, 1, 0)
