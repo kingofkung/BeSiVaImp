11636872ab22c4092d173daccb0be1977ae2cb34
Ben Rogers <bjroger@ku.edu>
2016-04-13 09:46:18 -0500

    Fixed it back to classification table style predictions

--- R/GSSDatAnalyze.R
+++ R/GSSDatAnalyze.R
@@ -97,7 +97,7 @@ if(exists("predictions")) rm(predictions)
 predictions <- lapply(glms, function(x)  try( predictr( x  , data = dat2, rowstouse = test)) )
 
 
-pcps <- unlist(lapply(predictions, function(x)  getpcp(x, dat2$vote12bin[test], fullpreds = FALSE) ))
+pcps <- unlist(lapply(predictions, function(x)  getpcp(x, dat2$vote12bin[test], fullpreds = TRUE) ))
 
 IVs <- unlist(lapply(formulae, function(x) as.character(x)[3]))
 
@@ -127,22 +127,22 @@ prop.table(table(dat2$vote12bin))
 ## I got tired of it telling us it's done.
 ## /System/Library/PrivateFrameworks/ScreenReader.framework/Versions/A/Resources/Sounds
 
-convmod <- glm(vote12bin ~ partyid + degree + race + age + income, data = dat2[-test,])
+## convmod <- glm(vote12bin ~ partyid + degree + race + age + income, data = dat2[-test,])
 
-convpreds <- predictr(convmod, dat2[], test)
-getpcp(convpreds, dat2[test,"vote12bin"])
+## convpreds <- predictr(convmod, dat2[], test)
+## getpcp(convpreds, dat2[test,"vote12bin"])
 
-m1 <- glm(vote12bin ~ vote08, data = dat2[-test,])
-m2 <- update(m1, .~. + educ)
+## m1 <- glm(vote12bin ~ vote08, data = dat2[-test,])
+## m2 <- update(m1, .~. + educ)
 
-m1.1 <- update(m1, .~. -vote08 + partyid)
-summary(m1.1)
-m2.1 <- update(m1.1, .~. + degree)
-m3.1 <- update(m2.1, .~. +sex + race)
-m4.1 <- update(m3.1, .~. + rplace)
-library(rockchalk)
-outreg(list("Iteration 1" = m1, "Iteration 2" = m2), title = "this is a test")
-outreg(list("Iteration 1" = m1.1, "Iteration 2" = m2.1, "Iteration 3" = m3.1, "Iteration 4" = m4.1), title = "Algorithm predictions, sans prior vote"   )
+## m1.1 <- update(m1, .~. -vote08 + partyid)
+## summary(m1.1)
+## m2.1 <- update(m1.1, .~. + degree)
+## m3.1 <- update(m2.1, .~. +sex + race)
+## m4.1 <- update(m3.1, .~. + rplace)
+## library(rockchalk)
+## outreg(list("Iteration 1" = m1, "Iteration 2" = m2), title = "this is a test")
+## outreg(list("Iteration 1" = m1.1, "Iteration 2" = m2.1, "Iteration 3" = m3.1, "Iteration 4" = m4.1), title = "Algorithm predictions, sans prior vote"   )
 
 
 system("afplay /System/Library/Sounds/Hero.aiff")
