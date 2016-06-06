32366cdbda3fedafc2aed771ea0df89552da3448
kingofkung <bjroger@ku.edu>
2016-06-06 15:03:40 -0500

    Begin work on toy dataset for algorithm to try

---
+++ R/MakeFunDataset.R
@@ -0,0 +1,16 @@
+## Just Create a Toy Dataset
+
+## Set the seed and select sample size
+set.seed(123456789)
+sampsize <- 25
+
+## Make some fake variables
+catmaker <- function(x, num = sampsize){letters[sample(x, num, replace = T)]}
+binmaker <- function(num = sampsize, ntrial = 1, prob = .5){rbinom(num, ntrial, prob)}
+
+cont1 <- rnorm(sampsize)
+cont2 <- sample(1:100, sampsize)
+
+cats <- sapply(rep(2:5,2), function(x) catmaker(1:x))
+binars <- sapply(seq(0.1, .9, .1), function(x) binmaker(prob = x))
+(cats, binars)
