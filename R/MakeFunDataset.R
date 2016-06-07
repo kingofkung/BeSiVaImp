ce5e5c0353a6e77045a3ecb4dbe7dc182ef1a23e
kingofkung <bjroger@ku.edu>
2016-06-07 14:26:55 -0500

    wrote up remainder of fun dataset

--- R/MakeFunDataset.R
+++ R/MakeFunDataset.R
@@ -13,4 +13,13 @@ cont2 <- sample(1:100, sampsize)
 
 cats <- sapply(rep(2:5,2), function(x) catmaker(1:x))
 binars <- sapply(seq(0.1, .9, .1), function(x) binmaker(prob = x))
-(cats, binars)
+daf <- data.frame(cats, binars, cont1, cont2)
+
+## Make my dependent variable
+lc <- 27 + 45 * cont1 - 12 *daf[14]
+pr <- (1 + exp(-lc))^-1
+yvar <- rbinom(sampsize, 1, pr)
+
+daf <- cbind(daf, yvar)
+
+
