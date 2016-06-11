fce9c569a62638550542166d656f405a9193133d
Ben Rogers <bjroger@ku.edu>
2016-06-10 23:16:43 -0500

    Demo for Tom and Dr. Doan

--- R/AnalyzeFunDataset.R
+++ R/AnalyzeFunDataset.R
@@ -11,7 +11,9 @@ colsofint <- colnames(daf)[!colnames(daf) %in% "yvar"]
 ## sample( nrow(daf), round(nrow(daf)/10) )
 
 
- for(i in 1:10) print(besiva('yvar', colsofint, daf, iters = 5, perc = .25, thresh = 0.01, sampseed = i  ))
+## for(i in 1:10)
+
+besiva('yvar', colsofint, daf, iters = 5, perc = .25, thresh = 0.0, sampseed = 54321)
 
 ## corrmod <- glm(yvar ~ X6.1 + cont1, data = daf, family = "binomial")
 ## summary(corrmod)
