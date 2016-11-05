0610419a876b3842a51000642abcf4e90b149023
Ben Rogers <bjroger@ku.edu>
2016-11-05 17:31:40 -0500

    Create file to start prepping ANES 2016 pilot study for analysis

---
+++ R/OpenANES2016.R
@@ -0,0 +1,9 @@
+## Open and get anes 2016 ready for analysis: On the enigma of donald trump
+library(foreign)
+
+if(!exists("anespilot")) anespilot <- read.spss("/Users/bjr/GitHub/BeSiVaImp/Data/anes_pilot_2016.sav", to.data.frame = T)
+
+head(anespilot)
+
+grep("timing", colnames(anespilot), value = T)
+grep("flag", colnames(anespilot), value = T)
